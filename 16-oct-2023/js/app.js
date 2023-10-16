256fe0b6565e1af1868b5734aa3af38ab6e8b312
abhinav3254 <abhinav3254@gmail.com>
2023-10-16 23:02:18 +0530

    transition

---
+++ 16-oct-2023/js/app.js
@@ -0,0 +1,18 @@
+gsap.to(
+    // class of the element
+    ".box",
+    {
+        // x axis
+        x: 500,
+        // y axis
+        y: 500,
+        // time it require to move
+        duration: 5,
+        // after loading page how much time should it wait
+        delay: 2,
+        // background color
+        backgroundColor: 'red',
+        // setting border while transition
+        border: 0
+    }
+)
