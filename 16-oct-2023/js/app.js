63b6ef50a4d78d7602449a2be229c634263fc16f
abhinav3254 <abhinav3254@gmail.com>
2023-10-16 23:11:26 +0530

    apply from

--- 16-oct-2023/js/app.js
+++ 16-oct-2023/js/app.js
@@ -5,14 +5,24 @@ gsap.to(
         // x axis
         x: 500,
         // y axis
-        y: 500,
+        y: 100,
         // time it require to move
         duration: 5,
         // after loading page how much time should it wait
-        delay: 2,
+        // delay: 2,
         // background color
         backgroundColor: 'red',
         // setting border while transition
-        border: 0
+        border: 0,
     }
 )
+
+
+gsap.from(".two", {
+    opacity: 0,
+    y: 100,
+    duration: 5,
+    x: 500,
+    // for infinite
+    repeat: -1
+});
